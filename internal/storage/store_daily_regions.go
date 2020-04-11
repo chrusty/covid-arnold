ae5e79e4ad9ce98d40c536a801d6a848b65ad5a5
chrusty <prawn@localhost>
2020-04-11 17:22:20 +1200

    poor-mans upserting

--- internal/storage/store_daily_regions.go
+++ internal/storage/store_daily_regions.go
@@ -6,18 +6,33 @@ import (
 
 // StoreDailyRegions saves a list of DailyRegions to the DB
 func (m *Manager) StoreDailyRegions(dailyRegions []*models.DailyRegion) error {
-	var rowsInserted int32
+	var rowsInserted int64
 
 	// Insert the records
 	for _, dailyRegion := range dailyRegions {
-		if err := m.dbConn.Create(dailyRegion).Error; err != nil {
-			m.logger.WithError(err).Warn("Error inserting a row")
-		} else {
-			rowsInserted++
+		response := m.dbConn.Set("gorm:insert_option", "ON CONFLICT DO NOTHING").Create(dailyRegion)
+		if response.Error != nil {
+			m.logger.WithError(response.Error).Warn("Error inserting a row")
 		}
+		rowsInserted += response.RowsAffected
 	}
 
 	m.logger.WithField("records_inserted", rowsInserted).Debug("Finished storing daily-regions records")
+
+	return nil
+}
+
+// StoreDailyRegionsBlob saves a list of DailyRegions to the DB
+func (m *Manager) StoreDailyRegionsBlob(dailyRegions []*models.DailyRegion) error {
+
+	// Insert the records
+	response := m.dbConn.Create(dailyRegions)
+	if response.Error != nil {
+		m.logger.WithError(response.Error).WithField("records_inserted", response.RowsAffected).Warn("Error inserting rows")
+		return response.Error
+	}
+
+	m.logger.WithField("records_inserted", response.RowsAffected).Debug("Finished storing daily-regions records")
 
 	return nil
 }
