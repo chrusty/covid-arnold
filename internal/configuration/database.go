d757f1f454f4ec06d3d17bfde77aa89f4523e493
chrusty <prawn@localhost>
2020-04-11 20:01:10 +1200

    Using a proper DB and username/password

--- internal/configuration/database.go
+++ internal/configuration/database.go
@@ -4,13 +4,13 @@ import "fmt"
 
 // Database configures a Postgres DB
 type Database struct {
-	Hostname string `env:"DB_PASSWORD" envDefault:"localhost"`
-	Password string `env:"DB_PASSWORD" envDefault:"changeme"`
-	Schema   string `env:"DB_SCHEMA" envDefault:"postgres"`
-	Username string `env:"DB_USER_NAME" envDefault:"postgres"`
+	DBName   string `env:"DB_SCHEMA" envDefault:"covid"`
+	Hostname string `env:"DB_HOSTNAME" envDefault:"localhost"`
+	Password string `env:"DB_PASSWORD" envDefault:"arnold"`
+	Username string `env:"DB_USER_NAME" envDefault:"covid"`
 }
 
 // ConnectionString returns a Postgres connection string based on the configured values
 func (d *Database) ConnectionString() string {
-	return fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", d.Hostname, d.Username, d.Schema, d.Password)
+	return fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", d.Hostname, d.Username, d.DBName, d.Password)
 }
